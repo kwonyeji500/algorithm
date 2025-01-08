191f8980d7400141737722a78edb46ed592a692c
yeji905 <77970912+kwonyeji500@users.noreply.github.com>
2025-01-09 08:42:34 +0900

    Java / 19.156315789473684ms

---
+++ PROGRAMMERS/131128_숫자 짝꿍.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String X, String Y) {
+        int[] countX = new int[10];
+        int[] countY = new int[10];
+
+        for (char c : X.toCharArray()) {
+            countX[c - '0']++;
+        }
+        for (char c : Y.toCharArray()) {
+            countY[c - '0']++;
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 9; i >= 0; i--) {
+            int commonCount = Math.min(countX[i], countY[i]);
+            for (int j = 0; j < commonCount; j++) {
+                result.append(i);
+            }
+        }
+
+        if (result.length() == 0) {
+            return "-1";
+        }
+
+        if (result.chars().allMatch(c -> c == '0')) {
+            return "0";
+        }
+
+        return result.toString();
+    }
+}

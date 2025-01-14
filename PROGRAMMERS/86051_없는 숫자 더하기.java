4c8f0f1b401f9425d038fd41549f92e2fd9482a4
yeji905 <77970912+kwonyeji500@users.noreply.github.com>
2025-01-15 08:24:14 +0900

    Java / 0.04666666666666666ms

---
+++ PROGRAMMERS/86051_없는 숫자 더하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        List<Integer> list = new ArrayList<>();
+        
+        for(int num : numbers) {
+            list.add(num);
+        }
+        
+        for (int i=0; i<10; i++) {
+            if(!list.contains(i)) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
